1cfe70d619061eab9251f3ac3fd67ccd0294b853
filipeboneberge <filipeboneberge@hotmail.com>
2024-06-16 16:54:51 -0300

    responsividade-links-scroll-to-top

--- src/javascript/scripts.js
+++ src/javascript/scripts.js
@@ -6,3 +6,38 @@ function menuMobile() {
 }
 
 menu.addEventListener("click", menuMobile);
+
+const elements = document.querySelectorAll(".hidden");
+
+const myObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+elements.forEach((element) => {
+  myObserver.observe(element);
+});
+
+let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
